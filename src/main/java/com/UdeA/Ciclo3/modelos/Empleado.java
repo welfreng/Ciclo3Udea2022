63747aa7e151986a0d1d3b04f58cd36503f3612c
welfreng <welfreng@gmail.com>
2022-08-17 17:59:55 -0500

    Crear Entidad EEmpleado y Movimiento Dinero

--- src/main/java/com/UdeA/Ciclo3/modelos/Empleado.java
+++ src/main/java/com/UdeA/Ciclo3/modelos/Empleado.java
@@ -1,4 +1,67 @@
 package com.UdeA.Ciclo3.modelos;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Empleado")
 public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String nombre;
+    private String correo;
+    private String rol;
+
+    @ManyToOne
+    private Empresa empresa;
+
+    public Empleado() {
+
+    }
+    public Empleado(int id, String nombre, String correo, String rol) {
+        this.id = id;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.rol = rol;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public Empresa getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
+    }
 }
--- src/main/java/com/UdeA/Ciclo3/modelos/MovimientoDinero.java
+++ src/main/java/com/UdeA/Ciclo3/modelos/MovimientoDinero.java
@@ -1,4 +1,14 @@
 package com.UdeA.Ciclo3.modelos;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "MovimientoDinero")
 public class MovimientoDinero {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    //private bigInteger monto;
+    //private bigInteger monto;
+
 }
