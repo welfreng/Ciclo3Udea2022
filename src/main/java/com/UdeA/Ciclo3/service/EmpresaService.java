f59f28d7b53769b44055d31b3578f2f26ef1087f
welfreng <welfreng@gmail.com>
2022-08-18 08:02:50 -0500

    Listar, Guardar y actualizar

--- src/main/java/com/UdeA/Ciclo3/service/EmpresaService.java
+++ src/main/java/com/UdeA/Ciclo3/service/EmpresaService.java
@@ -8,16 +8,35 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+
+//Le decimos a spring que esta clase es de servicio
 @Service
 public class EmpresaService {
     @Autowired  // se conecta con el repository empresaRepository
-    EmpresaRepository empresaRepository;
+    EmpresaRepository empresaRepository; //creamos un objeto tipo empresa para usar los metodos
 
+    //Metodo que reornara la lista de empresa usando heredados del JpaRepository
     public List<Empresa> getEmpresa(){
         List<Empresa> empresaList = new ArrayList<>();
         empresaRepository.findAll().forEach(Empresa -> empresaList.add(Empresa));
 
         return empresaList;
     }
+
+    //metodo que me trae un objeto de tipo empresa
+    public Empresa getEmpresabyId(Integer id) {
+        return empresaRepository.findById(id).get();
+    }
+
+    //Metodo para guardar o actualizar objetos de tipo empresa
+    public boolean saveOrUpdate(Empresa empresa)  {
+        Empresa emp = empresaRepository.save(empresa);
+        if (empresaRepository.findById(emp.getId()) != null) {
+            return true;
+        }
+        return false;
+    }
+
+    //metodo delete pendiente
 
 }
