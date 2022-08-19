fa355b8a26b6fbb3f4d0b8fdd05e4795ea610e98
welfreng <welfreng@gmail.com>
2022-08-19 07:04:31 -0500

    creacion del archivo controllerFull para listar empresas

--- src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
+++ src/main/java/com/UdeA/Ciclo3/Ciclo3Application.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfi
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Ciclo3Application {
 
---
+++ src/main/java/com/UdeA/Ciclo3/controller/ControllerFull.java
@@ -0,0 +1,25 @@
+package com.UdeA.Ciclo3.controller;
+
+import com.UdeA.Ciclo3.modelos.Empresa;
+import com.UdeA.Ciclo3.service.EmpresaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class ControllerFull {
+    @Autowired  //el cable que se conecta con la clase deseada
+   EmpresaService empresaService;
+
+
+    @GetMapping({"/Empresas", "/verEmpresa"})
+    public String viewEmpresas(Model model) {
+        List<Empresa> listEmpresas = empresaService.getEmpresa();
+        model.addAttribute("emplist", listEmpresas);
+
+        return "verEmpresa";  //aqui devolvemos a la pagina pero debemos ingresarla al template para cuando creemos el html
+    }
+}
